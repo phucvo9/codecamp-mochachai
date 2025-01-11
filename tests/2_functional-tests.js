f2adbbf7208ae51f20331325d756b0cb7d25d5fa
Phuc Vo <vhphuc.it@gmail.com>
2025-01-11 03:35:45 +0000

    fix #5

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -127,7 +127,7 @@ suite('Functional Tests with Zombie.js', function () {
     // assert that the text inside the element 'span#surname' is 'Colombo'
     browser.assert.text('span#surname', 'Colombo');
     // assert that the element(s) 'span#dates' exist and their count is 1
-    browser.assert.element('span#dates', 1);
+    browser.assert.elementCount('span#dates', 1);
     
     done();
     });
