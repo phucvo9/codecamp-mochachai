cd319d072de25118b2bc2b902d4946971fae6f1e
Phuc Vo <vhphuc.it@gmail.com>
2025-01-11 05:36:32 +0000

    fix local

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -71,6 +71,7 @@ suite('Functional Tests', function () {
 
 const Browser = require('zombie');
 Browser.site = 'https://boilerplate-mochachai.1nfernox.repl.co';
+Browser.site = 'http://localhost:3000';
 
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
