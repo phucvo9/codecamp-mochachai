a6e78613d692fb675016bfbcc049df1218b292c4
Phuc Vo <vhphuc.it@gmail.com>
2025-01-11 03:47:12 +0000

    #6

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -131,15 +131,22 @@ suite('Functional Tests with Zombie.js', function () {
     
     done();
     });
-  });
+  
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
+      browser.assert.success();
+    // assert that the text inside the element 'span#name' is 'Amerigo'
+    browser.assert.text('span#name', 'Amerigo');
+    // assert that the text inside the element 'span#surname' is 'Vespucci'
+    browser.assert.text('span#surname', 'Vespucci');
+    // assert that the element(s) 'span#dates' exist and their count is 1
+    browser.assert.element('span#dates', 1);
 
       done();
     });
   });
   });
+});
 });
 
 
